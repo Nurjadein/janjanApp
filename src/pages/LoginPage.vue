72db51fe4ce32e72ff63e5155cf1eba15169287e
Nurjadein <nurjadeinabdulmorid@gmail.com>
2022-05-09 07:15:05 +0800

    rename index as login page

---
+++ src/pages/LoginPage.vue
@@ -0,0 +1,10 @@
+<template>
+  <q-page class="row items-center justify-evenly"> </q-page>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+
+@Options({})
+export default class LoginPage extends Vue {}
+</script>
