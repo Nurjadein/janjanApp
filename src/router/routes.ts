c70cbb1986efc791978f947eac4b26055d932be5
Nurjadein <nurjadeinabdulmorid@gmail.com>
2022-05-09 07:15:32 +0800

    updating route

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -3,8 +3,10 @@ import { RouteRecordRaw } from 'vue-router';
 const routes: RouteRecordRaw[] = [
   {
     path: '/',
-    component: () => import('layouts/MainLayout.vue'),
-    children: [{ path: '', component: () => import('pages/Index.vue') }],
+    component: () => import('layouts/PlainLayout.vue'),
+    children: [
+      { path: '', component: () => import('src/pages/LoginPage.vue') },
+    ],
   },
 
   // Always leave this as last one,
